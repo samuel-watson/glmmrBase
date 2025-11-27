b076b98d159bbb5807243ac51d799ddc7179a931
Samuel Watson (Applied Health Sciences) <s.i.watson@bham.ac.uk>
2025-11-27 12:26:14 +0000

    v1.2.0 Fix reml 2

--- inst/include/glmmr/modelmatrix.hpp
+++ inst/include/glmmr/modelmatrix.hpp
@@ -1286,7 +1286,7 @@ inline void glmmr::ModelMatrix<modeltype>::posterior_u_samples(const int niter,
         W_ = exp_eta.matrix();
         ymod = (eta + (model.data.y.array() - exp_eta) / exp_eta).matrix();
       }
-      
+      WZL.noalias() = (ZL.array().colwise() * W_.array()).matrix();
       if(reml){
         WX.noalias() = (X.array().colwise() * W_.array()).matrix();
         MatrixXd XWX = X.transpose() * WX;
@@ -1294,10 +1294,10 @@ inline void glmmr::ModelMatrix<modeltype>::posterior_u_samples(const int niter,
         newW.setZero();
         newW.diagonal() += W_;
         newW -= WX * XWX * WX.transpose();
-        WZL.noalias() = ZLt * newW;
-        LWL.noalias() = WZL * ZL;
+        //WZL.noalias() = ZLt * newW;
+        LWL.noalias() = ZLt * newW * ZL;
       } else {
-        WZL.noalias() = (ZL.array().colwise() * W_.array()).matrix();
+        
         LWL.noalias() = ZLt * WZL;
       }
       LWL.diagonal().array() += 1.0;
